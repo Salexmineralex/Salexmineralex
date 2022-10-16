2e8b2604d233a2612a70718bd391c1b51171ebc1
Salexmineralex <74661819+Salexmineralex@users.noreply.github.com>
2022-10-16 12:58:02 +0200

    Cambios Portfolio

---
+++ Components/project_component.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const e = React.createElement;
+
+class project_component extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked comment number ' + this.props.commentID;
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    // Read the comment ID from a data-* attribute.
+    const commentID = parseInt(domContainer.dataset.commentid, 10);
+    const root = ReactDOM.createRoot(domContainer);
+    root.render(
+      e(LikeButton, { commentID: commentID })
+    );
+  });
