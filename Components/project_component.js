6d8b3512707a144038135efba057ccaae593ef4c
Salexmineralex <74661819+Salexmineralex@users.noreply.github.com>
2023-03-10 17:34:16 +0100

    Avances varios

--- Components/project_component.js
+++ Components/project_component.js
@@ -1,6 +1,14 @@
 'use strict';
 
+
 class Slider extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: null
+    }
+  }
   componentDidMount() {
     if (typeof swiffyslider !== 'undefined') {
       // the variable is defined
@@ -14,25 +22,26 @@ class Slider extends React.Component {
     .then((response) => response.json())
     .then((json) => {
       const projects = Object.values(json);
-
       const projectData = projects.find((project) => project.Name === this.props.project);
-  
-      this.crearSliders(projectData);
+      this.setState({data: projectData})
     });
   }
 
-  crearSliders(data) {
+  
 
+  crearSliders(data) {
+   
     if (!data) {
       return null; // or some other fallback option
     }
     const photos = data.PhotoRoute;
    
     const slides = [];
+
   
     if (photos && photos.length > 0) {
       photos.forEach((photo, index) => {
-
+        console.log(photo)
         const slide = (
           <li key={index}>
             <img
@@ -44,14 +53,16 @@ class Slider extends React.Component {
           </li>
         );
         slides.push(slide);
+       
       });
 
-   
     }
 
+ 
     return (
+      
       <div className="swiffy-slider slider-nav-round slider-nav-animation slider-nav-animation-fadein slider-item-first-visible">
-        <ul className="slider-container">{slides}</ul>
+        <ul className="slider-container">{this.state.slides}</ul>
         <button type="button" className="slider-nav" aria-label="Go left" />
         <button type="button" className="slider-nav slider-nav-next" aria-label="Go left" />
         <div className="slider-indicators">
@@ -61,11 +72,11 @@ class Slider extends React.Component {
         </div>
         <div className="card-body p-1 p-md-3 p-xl-5">
           <h2 className="card-title" style={{ color: "#DF1E1E" }}>
-            {data.Name}
+            {this.state.data.Name}
           </h2>
-          <p className="card-text mt-3">{data.Description}</p>
+          <p className="card-text mt-3">{this.state.data.Description}</p>
           <p className="card-text">
-            <small className="text-muted">{data.Lenguaje}</small>
+            <small className="text-muted">{this.state.data.Lenguaje}</small>
           </p>
         </div>
       </div>
@@ -73,9 +84,11 @@ class Slider extends React.Component {
   }
 
   render() {
+
+    if(!this.state.data) return <div>Loading slides, please wait .....</div>
     return (
       <div>
-        {this.crearSliders(this.props.data)}
+        {this.crearSliders(this.props.project)}
       </div>
     );
   }
