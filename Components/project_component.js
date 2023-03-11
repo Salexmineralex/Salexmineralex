aeebf966cdca74fb53e850569b80edbb265f648d
Salexmineralex <74661819+Salexmineralex@users.noreply.github.com>
2023-03-11 12:54:31 +0100

    LotOfChanges

--- Components/project_component.js
+++ Components/project_component.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 class Slider extends React.Component {
 
   constructor(props) {
@@ -60,9 +59,51 @@ class Slider extends React.Component {
 
  
     return (
-      
-      <div className="swiffy-slider slider-nav-round slider-nav-animation slider-nav-animation-fadein slider-item-first-visible">
-        <ul className="slider-container">{this.state.slides}</ul>
+      <div className="swiffy-slider slider-nav-round slider-nav-animation slider-nav-animation-fadein" >
+          <ul className="slider-container">
+          {this.state.data.PhotoRoute.map((photo, index) => 
+            <li className="slide-visible" key={index}>
+              <div className="card rounded-0 h-100" >
+                  <div className="row g-0 h-100">
+                      <div className="col-md-6 col-xl-5 d-flex align-items-center p-2 p-md-3 p-xl-5">
+                          <div className="card-body p-1 p-md-3 p-xl-5">
+                              <p className="lead"></p>
+                              <h2 className="card-title" style={{ color: '#DF1E1E' }} >{this.state.data.Name}</h2>
+                              <p className="card-text mt-3">Super fast lightweight slider and carousel with amazing touch support and user experience.</p>
+                              <p>Super simple setup using just markup and few powerful configuration options</p>
+                              <p className="card-text"><small className="text-muted">Remember to check out on mobile</small></p>
+                          </div>
+                      </div>
+                      <div className="col-md-6 col-xl-7">
+                          <img  src={photo} className={`card-img d-none d-md-block${index === 0 ? " slide-visible" : ""}`} loading="lazy" style={{ height: '100%',objectFit:'scale-down'}} alt="..."/>
+                          <img  src={photo} className={`card-img d-none d-md-none${index === 0 ? " slide-visible" : ""}`} loading="lazy" style={{ width: '100%',objectFit:'scale-down'}} alt="..."/>
+                      </div>
+                  </div>
+              </div>
+          </li>)}
+        </ul>
+        <button type="button" className="slider-nav" aria-label="Go left"></button>
+        <button type="button" className="slider-nav slider-nav-next" aria-label="Go left"></button>
+
+        <div className="slider-indicators">
+          <button className="" aria-label="Go to slide"></button>
+          <button aria-label="Go to slide" className="active"></button>
+          <button aria-label="Go to slide"></button>
+    </div>
+      </div>);
+  }
+
+      /*{ <div className="swiffy-slider slider-nav-round slider-nav-animation slider-nav-animation-fadein slider-item-first-visible">
+         <ul className="slider-container"> {this.state.data.PhotoRoute.map((photo, index) => 
+          <li key={index}>
+            <img
+              src={photo}
+              className={`card-img d-none d-md-block${index === 0 ? " slide-visible" : ""}`}
+              loading="lazy"
+              alt="..."
+            />
+          </li>)}
+         </ul>
         <button type="button" className="slider-nav" aria-label="Go left" />
         <button type="button" className="slider-nav slider-nav-next" aria-label="Go left" />
         <div className="slider-indicators">
@@ -79,9 +120,7 @@ class Slider extends React.Component {
             <small className="text-muted">{this.state.data.Lenguaje}</small>
           </p>
         </div>
-      </div>
-    );
-  }
+      </div> */
 
   render() {
 
